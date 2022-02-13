6bd362ba375b3abf7b3d38f33f0f5a6ed890cb64
Diego Souza <diegomagasouza@gmail.com>
2022-02-13 14:14:37 -0300

    update

--- script.sh
+++ script.sh
@@ -11,7 +11,24 @@
 # | SOME CONFIGURATION  |
 # -----------------------
 mkdir ~/.themes
+mkdir ~/.icons
+wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 wget -c https://dl3.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTY0NDQ0MjczNCwidSI6bnVsbCwibHQiOiJkb3dubG9hZCIsInMiOiI4NGFjNTk0MTk5ODIxNTRlZmM5NDZmNzA5ZWJjOGEyZDRmZDcyNGQ3OThmMzg3OTkxNzc3NDdhNzhkYmVkZmZiYmMxZThlYjBkNDUxNDVhZGUxOWI1OTRmNmU3ZGM2MmVhNjY2YmQ5YTNmMGNlNTBhZTdiMzdiZDUwNWFkN2JmMCIsInQiOjE2NDQ3MTkwNzcsInN0ZnAiOiI2ZmU0MzcxNTM4MWMwODIxNjA5YzM0ZTk0OGEzMmI5YSIsInN0aXAiOiI0NS4xOTEuMTM0LjIxIn0.19JhXORc_W79xPphNB4z3v8O5mihx4h7WTBwoygS-gk/Nordic-darker-v40.tar.xz && tar xf Nordic-darker-v40.tar.xz -C ~/.themes
+
+# VSCODE
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https
+
+# DOCKER
+curl -fsSL https://get.docker.com | bash
+
+# SPOTIFY
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 # -----------------------
 # |      ADD REPOS      |
@@ -36,6 +53,10 @@ gnome-tweak-tool \
 # gnome-shell-extension-dash-to-panel -y \
 # gnome-tweaks \
 papirus-icon-theme \
+default-jre -y \
+default-jdk -y \
+code \
+spotify-client
 
 sudo apt-get install terminator
 
@@ -45,4 +66,4 @@ sudo apt-get install terminator
 # -----------------------
 
 # ADDING NEOFETCH WHEN TERMINAL OPENS
-# echo neofetch >> ~/.bashrc
+echo neofetch >> ~/.bashrc
