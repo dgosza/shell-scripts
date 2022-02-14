f10c1cb143bea81fda44627a94195f31c531d64d
Diego Souza <diegomagasouza@gmail.com>
2022-02-14 19:04:02 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,33 +10,11 @@
 # https://pointcom.sampa.br/linux/como-instalar-o-vmware-workstation-player-no-ubuntu-20-04/
 
 # -----------------------
-# |  PRE INSTALLATION   |
-# -----------------------
-
-
-# -----------------------
 # | SOME CONFIGURATION  |
 # -----------------------
 sudo apt-get install curl -y
 mkdir ~/.themes
 mkdir ~/.icons
-
-# ICONS PAPIRUS
-wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh
-
-# NVM
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-# TeamViewer
-sudo sh -c "echo 'deb http://linux.teamviewer.com/deb stable main' >> /etc/apt/sources.list.d/teamviewer.list"
-sudo sh -c "echo 'deb http://linux.teamviewer.com/deb preview main' >> /etc/apt/sources.list.d/teamviewer.list"
-wget -q https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc -O- | sudo apt-key add -
-
-# DOCKER
-curl -fsSL https://get.docker.com | bash
-
-# ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # -----------------------
 # |      ADD REPOS      |
@@ -75,9 +53,19 @@ notion-snap
 sudo snap install code --classic
 sudo snap install ant --classic
 
-# -----------------------
-# | SOME CONFIGURATION  |
-# -----------------------
+# ICONS PAPIRUS
+wget -qO- https://git.io/papirus-icon-theme-install | DESTDIR="$HOME/.icons" sh
+
+# NVM
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# TeamViewer
+sudo sh -c "echo 'deb http://linux.teamviewer.com/deb stable main' >> /etc/apt/sources.list.d/teamviewer.list"
+sudo sh -c "echo 'deb http://linux.teamviewer.com/deb preview main' >> /etc/apt/sources.list.d/teamviewer.list"
+wget -q https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc -O- | sudo apt-key add -
+
+# DOCKER
+curl -fsSL https://get.docker.com | bash
 
 # VMWARE
 wget --user-agent="Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0" https://www.vmware.com/go/getplayer-linux
@@ -85,23 +73,24 @@ chmod +x getplayer-linux
 sudo ./getplayer-linux --required --eulas-agreed
 rm -f getplayer-linux
 
-# SPACESHIP THEME TO ZSH
-git clone https://github.com/denysdovhan/spaceship-prompt.git ~/.oh-my-zsh/custom/themes/spaceship-prompt
-ln -s ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
-
 # -----------------------
 # |  POST INSTALLATION  |
 # -----------------------
 
-# Install FireCode Font in Terminus. 
-# INSTALL FONT https://github.com/tonsky/FiraCode/releases
-
-# gedit ~/.zshrc
-# ZSH_THEME="spaceship"
-
 # THEME NORDIC
 # https://www.gnome-look.org/p/1267246
 # tar xf Nordic-darker-v40.tar.xz -C ~/.themes
+
+# ZSH BASH AND SPACESHIP THEME 
+# Install font https://github.com/tonsky/FiraCode/releases
+# Install FireCode Font in Terminator. 
+
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# git clone https://github.com/denysdovhan/spaceship-prompt.git ~/.oh-my-zsh/custom/themes/spaceship-prompt
+# ln -s ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
+
+# gedit ~/.zshrc
+# ZSH_THEME="spaceship"
 
 # ADDING NEOFETCH WHEN TERMINAL OPENS
 # echo neofetch >> ~/.bashrc
