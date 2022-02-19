f46e8fd32fae5e484fce9962d70e3a38a140f8e0
Diego Souza <diegomagasouza@gmail.com>
2022-02-19 02:40:32 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -22,6 +22,8 @@ mkdir ~/.icons
 # -----------------------
 sudo add-apt-repository ppa:papirus/papirus
 sudo add-apt-repository universe
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
 # -----------------------
 # |    UPDATE SOURCES   |
@@ -35,6 +37,7 @@ sudo apt-get install \
 neofetch \
 tree \
 gnome-tweak-tool \
+google-chrome-stable \
 papirus-icon-theme \
 default-jre \
 default-jdk \
